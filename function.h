37c3d0ebed0a1f86cfdbb77caf6d49100a6b40d4
Guilherme Licodiedoff <guiliff@protonmail.com>
2025-07-13 23:53:40 -0300

    add

--- function.h
+++ function.h
@@ -2,10 +2,11 @@
 #define RIGHT 'd'
 #define LEFT 'a'
 #define DOWN 's'
+#define BOMBA 'b'
 
 void move(char direction);
 int finish();
 
 int intdirection(char direction);
-
+void explodepilula();
 void ghost();
