0ef5052781466c18ec21646e36f8991bd8818b15
Guilherme Licodiedoff <guiliff@protonmail.com>
2025-07-13 23:52:42 -0300

    add

--- map.h
+++ map.h
@@ -3,6 +3,7 @@
 #define EMPTY '.'
 #define VERTICAL_WALL '|'
 #define HORIZONTAL_WALL '-'
+#define PILULA '+'
 
 //Criando uma estrutura para manter o mapa, facilitando caso venha a expandir o código com mais mapas
 struct map{
@@ -22,8 +23,12 @@ void dynamic_allocation(MAPA* m);
 void seemap(MAPA* m);
 void freemap(MAPA* m);
 void printmap(MAPA* m);
-void findmap(MAPA* m, POSITION* p, char c);
+int findmap(MAPA* m, POSITION* p, char c);
 int valid(MAPA* m, int x, int y);
 int isempty (MAPA* m, int x, int y);
 int movemap(MAPA* m, int xorigem, int yorigem, int xdestino, int ydestino);
 void copymap(MAPA* destino, MAPA* origem);
+int positionghost(int xatual, int yatual, int* xdestino, int*ydestino);
+int podeandar(MAPA* m, char personagem, int x, int y);
+int parede(MAPA* m, int x, int y);
+int ehpersonagem(MAPA* m, char personagem, int x, int y);
