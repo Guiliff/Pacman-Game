3cebf23ce9e5908372a93b0d0e361f2f253ebefd
Guilherme Licodiedoff <guiliff@protonmail.com>
2025-07-13 23:52:03 -0300

    add

--- pacman_game.c
+++ pacman_game.c
@@ -2,9 +2,33 @@
 #include <stdlib.h>
 #include "function.h"
 #include "map.h"
+#include <time.h>
 
 MAPA m;
 POSITION hero;
+int tempilula = 0;
+
+int positionghost(int xatual, int yatual, int* xdestino, int*ydestino){
+    int opcoes[4][2] = {
+        {xatual, yatual +1},
+        {xatual +1, yatual},
+        {xatual, yatual -1},
+        {xatual -1, yatual}
+    };
+    srand(time(0));
+    for (int i = 0; i<10; i++){
+        int posicao = rand() %4;
+
+        if(podeandar(&m, GHOST, opcoes[posicao][0], opcoes[posicao][1])){
+            *xdestino = opcoes[posicao][0];
+            *ydestino = opcoes[posicao][1];
+
+            return 1;
+        }
+    }
+
+    return 0;
+}
 
 void ghost(){
     MAPA copy;
@@ -14,8 +38,14 @@ void ghost(){
     for(int i = 0; i<m.linhas; i++){
         for (int j=0; j<m.colunas;j++){
             if(copy.matrix[i][j] == GHOST){
-                if (valid(&m, i, j+1) && isempty(&m, i, j+1)){
-                    movemap(&m, i, j, i, j+1);
+
+                int xdestino;
+                int ydestino;
+
+                int encontrou = positionghost(i, j, &xdestino, &ydestino);
+
+                if (encontrou){
+                    movemap(&m, i, j, xdestino, ydestino);
                 }
             }
         }
@@ -24,7 +54,9 @@ void ghost(){
 }
 
 int finish(){
-    return 0;
+    POSITION pos;
+    int fogefogenomapa = findmap(&m, &pos, HERO);
+    return !fogefogenomapa;
 }
 
 int intdirection(char direction){
@@ -58,16 +90,20 @@ void move(char direction){
             break;
     }
 
-    if(!valid(&m, proximox, proximoy))
+    if(!podeandar(&m, HERO, proximox, proximoy))
         return;
 
-    if (!isempty(&m, proximox, proximoy))
-        return;
+    if(ehpersonagem(&m, PILULA, proximox, proximoy))
+        tempilula = 1;;
 
     movemap(&m, hero.x, hero.y, proximox, proximoy);
     hero.x = proximox;
     hero.y = proximoy;
 
+}
+
+void explodepilula(){
+    printf("explodiu");
 }
 
 int main(){
@@ -77,11 +113,16 @@ int main(){
     findmap(&m, &hero, HERO);
 
     do{
+
+        printf("Tem pilula: %s\n", (tempilula ? "SIM" : "NAO"));
+        
         printmap(&m);
 
         char user_command;
         scanf(" %c", &user_command);
         move(user_command);
+        if (user_command == BOMBA) explodepilula();
+
         ghost();
     }
     while(!finish());
